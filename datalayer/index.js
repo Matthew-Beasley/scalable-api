66adcfabb0d1ca43b554414762e44d8da8b78458
conbec <56614504+conbec@users.noreply.github.com>
2020-02-20 13:46:22 -0800

    datalayer/shelters done(ish), not tested

--- datalayer/index.js
+++ datalayer/index.js
@@ -6,4 +6,4 @@ module.exports = {
   readDog,
   createShelter,
   readShelter
-}
+};
---
+++ datalayer/shelters.js
@@ -0,0 +1,18 @@
+const { client } = require('./client');
+
+const createShelter = async ({ shelterName }) => {
+  const sql = 'INSERT INTO shelters (shelter_name) VALUES ($1) RETURNING *';
+  const data = await client.query(sql, [shelterName]);
+  return data.rows;
+}
+
+const readShelter = async () => {
+  const sql = 'SELECT * FROM shelters RETURNING *';
+  const data = await client.query(sql);
+  return data.rows;
+}
+
+module.exports = {
+  createShelter,
+  readShelter
+}
