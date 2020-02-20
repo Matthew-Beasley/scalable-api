220f80585af52f8ccd644dbe79e55db6cb22370b
conbec <56614504+conbec@users.noreply.github.com>
2020-02-20 11:56:56 -0800

    data setup done

---
+++ datalayer/setup.sql
@@ -0,0 +1,14 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+DROP TABLE IF EXISTS dogs;
+DROP TABLE IF EXISTS shelters;
+
+CREATE TABLE shelters (
+  shelter_id UUID PRIMARY KEY DEFAULT uuid_generate_v1(),
+  shelter_name VARCHAR(100)
+);
+
+CREATE TABLE dogs (
+  dog_id UUID PRIMARY KEY DEFAULT uuid_generate_v1(),
+  dog_name VARCHAR(100) NOT NULL,
+  shelter_id UUID REFERENCES shelters(shelter_id)
+);
