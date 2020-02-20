c5f99fee866bb3a02ff058a283213dd2e7e8cb41
conbec <56614504+conbec@users.noreply.github.com>
2020-02-20 12:08:19 -0800

    datalayer index complete

---
+++ datalayer/index.js
@@ -0,0 +1,9 @@
+const { createDog, readDog } = require('./dogs');
+const { createShelter, readShelter } = require('./shelters');
+
+module.exports = {
+  createDog,
+  readDog,
+  createShelter,
+  readShelter
+}
